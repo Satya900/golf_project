8d639f19da17eb8deef4444bb91083c1491cc52c
Satyabrata Mohanty <92725722+Satya900@users.noreply.github.com>
2026-03-31 00:04:50 +0530

    Update frontend/src/components/ui/button.jsx

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -31,7 +31,7 @@ const buttonVariants = cva(
 )
 
 const Bton = React.forwardRef(({ className, variant, si asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button"
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   return (
     <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
   )
